ee2578c8982a63704229a11dfe4fc3e1256cd527
Amuda Rasheed <amudarash102@gmail.com>
2025-12-12 11:34:28 +0100

    Remove cached views/sessions from tracking and update .gitignore to ignore storage/framework generated files

--- storage/framework/views/32ef95ca7b755a17ab43887f50639108.php
+++
@@ -1,3 +0,0 @@
-
-<input type="hidden" value="<?php echo e($template); ?>" name="email_template">
-<?php /**PATH C:\xampp\htdocs\mytijaara\resources\views/admin-views/business-settings/email-format-setting/partials/email-template-section.blade.php ENDPATH**/ ?>
