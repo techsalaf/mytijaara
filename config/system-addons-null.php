8432729581bb5113f9235c51f6a1d0ae25c2f75f
Amuda Rasheed <amudarash102@gmail.com>
2025-12-17 14:40:20 +0100

    modified system addons

--- config/system-addons-null.php
+++ config/system-addons-null.php
@@ -54,3 +54,64 @@
     'software_type' => 'addon',
   ),
 );
+
+?>
+
+
+<?php return array (
+// edited BY GASHFIX
+  'admin_panel' => 
+  array (
+    'active' => '1',
+    'username' => 'techsalaf',
+    'purchase_key' => 'NulledMaster',
+    'software_id' => 'MzY3NzIxMTI=',
+    'domain' => 'kari.iqacademyng.com',
+    'software_type' => 'product',
+  ),
+  'vendor_panel' => 
+  array (
+    'active' => '1',
+    'username' => '',
+    'purchase_key' => '',
+    'software_id' => '',
+    'domain' => 'kari.iqacademyng.com',
+    'software_type' => 'addon',
+  ),
+  'user_app' => 
+  array (
+    'active' => '1',
+    'username' => '',
+    'purchase_key' => '',
+    'software_id' => '',
+    'domain' => 'kari.iqacademyng.com',
+    'software_type' => 'addon',
+  ),
+  'vendor_app' => 
+  array (
+    'active' => 1,
+    'username' => 'techsalaf',
+    'purchase_key' => 'NulledMaster',
+    'software_id' => 'MzY3NzIxNzM=',
+    'domain' => 'kari.iqacademyng.com',
+    'software_type' => 'addon',
+  ),
+  'deliveryman_app' => 
+  array (
+    'active' => 1,
+    'username' => 'techsalaf',
+    'purchase_key' => 'NulledMaster',
+    'software_id' => 'MzY3NzIxNDg=',
+    'domain' => 'kari.iqacademyng.com',
+    'software_type' => 'addon',
+  ),
+  'react_web' => 
+  array (
+    'active' => '1',
+    'username' => 'techsalaf',
+    'purchase_key' => 'NulledMaster',
+    'software_id' => 'NDUzNzAzNTE=',
+    'domain' => 'kari.iqacademyng.com',
+    'software_type' => 'addon',
+  ),
+);
